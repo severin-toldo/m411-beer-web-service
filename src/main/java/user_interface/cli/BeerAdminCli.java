0c91eb2835e2e00a9d3fad6372af4b8d8a4a9a05
stoldo <stoldo@runmyaccounts.com>
2019-11-05 13:02:23 +0100

    moved all sysos in main method since it is a requirement...

--- src/main/java/user_interface/cli/BeerAdminCli.java
+++ src/main/java/user_interface/cli/BeerAdminCli.java
@@ -9,13 +9,9 @@ import java.io.InputStreamReader;
 import main.java.model.Beer;
 
 public class BeerAdminCli {
-	
-	private BeerService beerService = new BeerService();
-
-	
     public static void main(String[] args) throws IOException {
     	boolean stop = false;
-    	BeerAdminCli admin = new BeerAdminCli();
+    	BeerService beerService = new BeerService();
     	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     	
     	while (!stop) {
@@ -36,21 +32,52 @@ public class BeerAdminCli {
         	
     		switch (decision) {
     			case "1":
-    		        admin.printBeerStyles();
+    				/*
+    				 * Erzeugt Konsolenausgabe der Bierarten. 
+    				 * Die id wird mit "::" vor dem Bierart Namen ausgeben.
+    				 * */
+    				beerService.getBeerStyles()
+    	    			.forEach((k, v) -> System.out.println(k + "::" + v));
     				break;
     			
     			case "2":
     				System.out.println("Bitte Suchwort eingeben: ");
-    				admin.printBeerStyles(reader.readLine());
+    				
+    				/*
+    				 *  Erzeugt Konsolenausgabe der Bierarten, welche die Zeichenfolge ″search″ im Namen enthalten. 
+    				 *  Die id wird mit "::" vor dem Bierart Namen ausgeben.
+    				 *  
+    				 *  @param searchString Zeichenfolge die enthalten sein soll
+    				 * */
+    				beerService.getBeerStylesBySearchStr(reader.readLine())
+    	    			.forEach((k, v) -> System.out.println(k + "::" + v));
     				break;
     				
     			case "3":
-    		        admin.printBeerList();
+    				/*
+    				 * Gibt zeilenweise ID und Name mit "::" getrennt der Biere aus.
+    				 * */
+    				beerService.getBeers()
+    	        		.forEach(x -> System.out.println(x.getId() + "::" + x.getName()));
     				break;
     				
     			case "4":
+    				/*
+    			     * Gibt in einer Zeile ID und Namen und in einer zweiten Zeile die Beschreibung 
+    			     * des entsprechenden Bieres aus.
+    			     * 
+    			     * @param id id nach der gesucht werden soll
+    			     * */
     				System.out.println("Bitte id eingeben: ");
-    		        admin.printBeer(reader.readLine());
+    		        String id = reader.readLine();
+    				Beer beer = beerService.getBeerById(id);
+    		        
+    		        if (beer != null) {
+    		        	System.out.println(beer.getId() + "::" + beer.getName());
+    		            System.out.println(beer.getDescription());	
+    		        } else {
+    		        	System.out.println("Kein Bier mit id " + id + " gefunden.");
+    		        }
     				break;
     			
     			case "0":
@@ -63,50 +90,5 @@ public class BeerAdminCli {
     				break;
     		}	
     	}
-    }
-
-	/**
-	 * Erzeugt Konsolenausgabe der Bierarten. 
-	 * Die id wird mit "::" vor dem Bierart Namen ausgeben.
-	 * */
-    public void printBeerStyles() {
-    	beerService.getBeerStyles()
-    		.forEach((k, v) -> System.out.println(k + "::" + v));
-    }
-    
-	/**
-	 *  Erzeugt Konsolenausgabe der Bierarten, welche die Zeichenfolge ″search″ im Namen enthalten. 
-	 *  Die id wird mit "::" vor dem Bierart Namen ausgeben.
-	 *  
-	 *  @param searchString Zeichenfolge die enthalten sein soll
-	 * */
-    public void printBeerStyles(String searchString) {
-    	beerService.getBeerStylesBySearchStr(searchString)
-    		.forEach((k, v) -> System.out.println(k + "::" + v));
-    }
-
-    /**
-     * Gibt zeilenweise ID und Name mit "::" getrennt der Biere aus.
-     * */
-    public void printBeerList() {
-        beerService.getBeers()
-        	.forEach(x -> System.out.println(x.getId() + "::" + x.getName()));
-    }
-    
-    /**
-     * Gibt in einer Zeile ID und Namen und in einer zweiten Zeile die Beschreibung 
-     * des entsprechenden Bieres aus.
-     * 
-     * @param id id nach der gesucht werden soll
-     * */
-    public void printBeer(String id) {
-        Beer beer = beerService.getBeerById(id);
-        
-        if (beer != null) {
-        	System.out.println(beer.getId() + "::" + beer.getName());
-            System.out.println(beer.getDescription());	
-        } else {
-        	System.out.println("Kein Bier mit id " + id + " gefunden.");
-        }
     }
 }
